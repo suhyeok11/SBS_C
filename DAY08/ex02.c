5297a19ee2807a14fd0b0247690997f8dfdd842b
suhyeok11 <suhyeok11@naver.com>
2022-01-26 14:27:18 +0900

    08

---
+++ DAY08/ex02.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+    # 포인터와 배열의 관계
+    - 배열의 이름 : 첫 요소의 주소값
+    - int arr[3];
+    - 벼열 요소의 접근하는 방법
+    -1. 인덱스 : arr[n]
+    -2. 포인터 : *(arr + n)
+
+*/
+int main(void)  {
+    int arr[3] = {1,2,3};
+
+    //인덱스를 통한 접근
+    printf("인덱스를 통한 접근\n");
+    printf("%d %d %d\n", arr[0], arr[1], arr[2]);
+
+    // 포인터 연산을 통한 배열 접근
+    printf("포인터 연산을  통한 배열 접근\n");
+    printf("%d %d %d\n", *(arr), *(arr+1), *(arr+2));
+
+    return 0;
+
+}
